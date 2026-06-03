44a122e609c192e4e5fb8713d7af73daf745d10d
Cooper Werner <cooperwerner824@gmail.com>
2026-06-02 21:24:30 -0400

    Setting Up the Firmware

---
+++ Firmware/Debug/CMakeFiles/Aurora-Watch.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/cooperwerner/Documents/personal-projects/Aurora-Watch/Firmware/startup_stm32u031xx.s" "/home/cooperwerner/Documents/personal-projects/Aurora-Watch/Firmware/Debug/CMakeFiles/Aurora-Watch.dir/startup_stm32u031xx.s.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32U031xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/home/cooperwerner/Documents/personal-projects/Aurora-Watch/Firmware/cmake/stm32cubemx/../../Core/Inc"
+  "/home/cooperwerner/Documents/personal-projects/Aurora-Watch/Firmware/cmake/stm32cubemx/../../Drivers/STM32U0xx_HAL_Driver/Inc"
+  "/home/cooperwerner/Documents/personal-projects/Aurora-Watch/Firmware/cmake/stm32cubemx/../../Drivers/STM32U0xx_HAL_Driver/Inc/Legacy"
+  "/home/cooperwerner/Documents/personal-projects/Aurora-Watch/Firmware/cmake/stm32cubemx/../../Drivers/CMSIS/Device/ST/STM32U0xx/Include"
+  "/home/cooperwerner/Documents/personal-projects/Aurora-Watch/Firmware/cmake/stm32cubemx/../../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/cooperwerner/Documents/personal-projects/Aurora-Watch/Firmware/Core/Src/main.c" "CMakeFiles/Aurora-Watch.dir/Core/Src/main.c.o" "gcc" "CMakeFiles/Aurora-Watch.dir/Core/Src/main.c.o.d"
+  "/home/cooperwerner/Documents/personal-projects/Aurora-Watch/Firmware/Core/Src/stm32u0xx_hal_msp.c" "CMakeFiles/Aurora-Watch.dir/Core/Src/stm32u0xx_hal_msp.c.o" "gcc" "CMakeFiles/Aurora-Watch.dir/Core/Src/stm32u0xx_hal_msp.c.o.d"
+  "/home/cooperwerner/Documents/personal-projects/Aurora-Watch/Firmware/Core/Src/stm32u0xx_it.c" "CMakeFiles/Aurora-Watch.dir/Core/Src/stm32u0xx_it.c.o" "gcc" "CMakeFiles/Aurora-Watch.dir/Core/Src/stm32u0xx_it.c.o.d"
+  "/home/cooperwerner/Documents/personal-projects/Aurora-Watch/Firmware/Core/Src/syscalls.c" "CMakeFiles/Aurora-Watch.dir/Core/Src/syscalls.c.o" "gcc" "CMakeFiles/Aurora-Watch.dir/Core/Src/syscalls.c.o.d"
+  "/home/cooperwerner/Documents/personal-projects/Aurora-Watch/Firmware/Core/Src/sysmem.c" "CMakeFiles/Aurora-Watch.dir/Core/Src/sysmem.c.o" "gcc" "CMakeFiles/Aurora-Watch.dir/Core/Src/sysmem.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Firmware/Debug/CMakeFiles/Aurora-Watch.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "Aurora-Watch.map"
+  "Aurora-Watch"
+  "Aurora-Watch.pdb"
+  "CMakeFiles/Aurora-Watch.dir/Core/Src/main.c.o"
+  "CMakeFiles/Aurora-Watch.dir/Core/Src/main.c.o.d"
+  "CMakeFiles/Aurora-Watch.dir/Core/Src/stm32u0xx_hal_msp.c.o"
+  "CMakeFiles/Aurora-Watch.dir/Core/Src/stm32u0xx_hal_msp.c.o.d"
+  "CMakeFiles/Aurora-Watch.dir/Core/Src/stm32u0xx_it.c.o"
+  "CMakeFiles/Aurora-Watch.dir/Core/Src/stm32u0xx_it.c.o.d"
+  "CMakeFiles/Aurora-Watch.dir/Core/Src/syscalls.c.o"
+  "CMakeFiles/Aurora-Watch.dir/Core/Src/syscalls.c.o.d"
+  "CMakeFiles/Aurora-Watch.dir/Core/Src/sysmem.c.o"
+  "CMakeFiles/Aurora-Watch.dir/Core/Src/sysmem.c.o.d"
+  "CMakeFiles/Aurora-Watch.dir/startup_stm32u031xx.s.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/Aurora-Watch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
